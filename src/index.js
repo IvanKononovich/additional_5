866138da1a0d136c1bcc94bdf753dbbd8cb33619
Ivan Kononovich <ivan.kononovich.12@gmail.com>
2021-03-13 10:25:02 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(originStr, bracketsConfig) {
+  let end = false;
+  let str = originStr
+  
+  while(!end) {
+    // We are looking for brackets that can be closed for sure and remove them
+    const findedBrackets = bracketsConfig.some(config => {
+      let oldString = str
+      str = str.replace(`${config[0]}${config[1]}`, '')
+      return oldString !== str
+    });
+
+    // Stop the loop if nothing has been removed
+    end = !findedBrackets
+  }
+
+  // If the str is empty, then all brackets were closed correctly
+  return !Boolean(str.length)
 }
